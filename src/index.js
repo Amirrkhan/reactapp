a4cdba72aae0ed3b01fd01582c6cfb3a3a3f8d73
Amirrkhan <aliamirovaxe@gmail.com>
2018-03-29 18:35:56 +0200

    Initial commit

--- src/index.js
+++ src/index.js
@@ -2,6 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'material-components-web/dist/material-components-web.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
